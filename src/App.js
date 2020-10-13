cb6a3c8501c349eac0550520e706f490ad33750b
My Name <myEmail@example.com>
2020-10-13 20:37:58 +0300

    master 2

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
