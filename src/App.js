1490affa0a54562e3198665861697c025e41ae8c
My Name <myEmail@example.com>
2020-10-13 21:54:55 +0300

    master 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 import './style.css';
 
 function App() {
-  return (
-    <>
-      <Header />
-
-      <section className="main">
-        <h1 className="title">
-          Falcon 1
-        </h1>
-
-        <div className="video-container">
-          <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+          <Header />
+          <Main />        
+        </>
+    );
 }
 
 export default App;
