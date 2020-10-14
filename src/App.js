102e70760853706c5c31f4471f0105346fbc2273
My Name <myEmail@example.com>
2020-10-14 22:42:00 +0300

    master 8

--- src/App.js
+++ src/App.js
@@ -6,16 +6,50 @@ import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
 //import Calendar from './components/Calendar/Calendar';
 //import Details from './components/Details/Details';
+import FetchData from './service/FetchData';
 
 import './style.css';
 
 class App extends React.Component {
+  
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketfeatures: null,
+    rockets: [],
+  };
+
+  componentDidMount() {
+    //this.fetchData.getCompany().then(data => console.log(data));
+    this.updateRocket();
+  }
+
+  
+
+  updateRocket() {
+    console.log(this.state)
+
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) });
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket)) // перебираем все рфкеты
+      .then(rocketFeatures => {
+        this.setState({ rocketFeatures }, () => {
+          console.log('после', this.state)
+        });// получаем рокету после фильтрации и обновляем
+      });
+    console.log('до', this.state);
+  }
 
   render() {
+    console.log(this.state)
     return (
       <>
-        <Header />
-        <Main />
+        <Header rockets={this.state.rockets} />
+        <Main rocket={this.state.rocket} />
         <Features />          
         <Footer />        
       </>
---
+++ src/service/FetchData.js
@@ -0,0 +1,27 @@
+// получение данных
+
+export default class FetchData {
+
+    startUrl = 'https://api.spacexdata.com/v4/';
+
+    // запрос к API
+    getResource = async url => { // пока не придет ответ
+        const res = await fetch(url); // не сможем записать результат
+
+        if (!res.ok) {
+            throw new Error (`Произошла ошибка ${res.status}` );
+        }
+        return await res.json();
+    };
+
+    // различные методы
+    getRocket = async () => 
+        await this.getResource(this.startUrl + 'rockets'); // ассинхронные, т.е. дожидаемся ответа
+
+    getLaunches = async () =>
+        await this.getResource(this.startUrl + 'launches/past');
+
+    getCompany = async () => 
+        await this.getResource(this.startUrl + 'company');
+
+}
