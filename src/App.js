47a5dced22ff3fa414d4d03108c9e52d86bacb03
My Name <myEmail@example.com>
2020-10-14 21:18:00 +0300

    master 7

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
+//import Calendar from './components/Calendar/Calendar';
+//import Details from './components/Details/Details';
 
 import './style.css';
 
-function App() {
+class App extends React.Component {
+
+  render() {
     return (
-        <>
-          <Header />
-          <Main />        
-        </>
+      <>
+        <Header />
+        <Main />
+        <Features />          
+        <Footer />        
+      </>
     );
+  }
 }
 
 export default App;
